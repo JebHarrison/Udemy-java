214fb72522c374bdaa2e2f9e947bb5c014560401
James Harrison <jamesebharrison@hotmail.com>
2022-07-29 11:10:41 +0100

    new practice files

---
+++ ByteShortIntLong.java
@@ -0,0 +1,37 @@
+public class ByteShortIntLong {
+    public static void main(String[] args) {
+        int myValue = 1000;
+        // int data types 
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Interger Minimum value = " + myMinIntValue);
+        System.out.println("Integer Maximum value = " + myMaxIntValue);
+        // byte data types 
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Min byte value is "+myMinByteValue);
+        System.out.println("Max byte value is "+myMaxByteValue);
+
+         // short data type 
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Min Short value is "+myMinShortValue);
+        System.out.println("Max Short value is "+myMaxShortValue);
+
+        long myLongValue = 100L;
+        Long myMinLongValue = Long.MIN_VALUE;
+        Long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Min Long value is "+myMinLongValue);
+        System.out.println("Max Long value is "+myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte mynewByteValue = (byte) (myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+      //  String message = "Hello World!";
+        System.out.println("Hello, James Harrison");
+
+        int myFirstNumber = (9 + 10) + (2 * 10);
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber = 12;
+        System.out.println(mySecondNumber);
+
+
+        int myThirdNumber = myFirstNumber * 2;
+        System.out.println(myThirdNumber);
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLast = 1000 - myTotal; 
+        System.out.println(myLast);
+    }
+}
